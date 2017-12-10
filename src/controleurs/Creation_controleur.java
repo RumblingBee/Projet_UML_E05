7f7be89177d362dd2698a43669a77013cca66dfc
Clement <clement.monestier@hotmail.fr>
2017-12-10 16:06:22 +0100

    Implementation ajout de Produits A TESTER

--- src/controleurs/Creation_controleur.java
+++ src/controleurs/Creation_controleur.java
@@ -6,18 +6,27 @@
 package controleurs;
 
 import javax.swing.JTextField;
+import metier.Catalogue;
+import metier.I_Catalogue;
 
 /**
  *
  * @author prax
  */
 public class Creation_controleur {
+    
+    private I_Catalogue Produits=new Catalogue();
 
-    public boolean creerPdt(JTextField nom, JTextField prixUnit, JTextField qteStock) {
+    public boolean creerPdt(JTextField sNom, JTextField sPrixUnit, JTextField sQteStock) {
+        String nom=sNom.getText();
         boolean prixValide;
-        prixValide = verifierPrix(prixUnit);
+        prixValide = verifierPrix(sPrixUnit);
         if(prixValide == true){
-            
+            if(produitExiste(nom)==false){
+                float prix = Float.parseFloat(sPrixUnit.getText());
+                int qte=Integer.parseInt(sQteStock.getText());
+                    return(Produits.addProduit(nom, prix, qte));
+            }
         }
         return false;
     }
@@ -34,6 +43,18 @@ public class Creation_controleur {
         } else {
             return true;
         }
+    }
+    
+    public boolean produitExiste(String nom){
+        boolean res=false;
+        String[] nomPdt=Produits.getNomProduits();
+        int i=0;
+        while(i<nomPdt.length && res==false){
+            if(nomPdt[i].equals(nom)){
+                res=true;
+            }
+        }
+        return res;
     }
 
 }
