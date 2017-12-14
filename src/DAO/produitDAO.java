9392d73c9944eacf3a3929d8fc03b12f59922ed2
Maxime <m.dezette@gmail.com>
2017-12-14 17:57:47 +0100

    creation findAll

--- src/DAO/produitDAO.java
+++ src/DAO/produitDAO.java
@@ -6,12 +6,70 @@
  */
 package DAO;
 
+import uml.e05.monestier.dezette.metier.Catalogue;
+import uml.e05.monestier.dezette.metier.I_Produit;
+import uml.e05.monestier.dezette.metier.Produit;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 /**
  *
  * @author clement
  */
 public class produitDAO {
-    
-   
-    
+
+    private Connection cn = null;
+    private ResultSet rs = null;
+    private Statement st = null;
+
+
+    private String url = "jdbc:oracle:thin:@162.38.222.149:1521:iut";
+    private String username = "monestierc";
+    private String password = "1105018199N";
+
+    public produitDAO(Connection cn) {
+
+
+     try{
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         try {
+             cn = DriverManager.getConnection(url,username,password);
+         }catch (SQLException e){
+             e.printStackTrace();
+         }
+
+     }catch (Exception e){
+         e.printStackTrace();
+     }
+
+    }
+
+
+
+    public ArrayList<I_Produit> findAll(){
+
+        ArrayList<I_Produit> arrayProduit = new ArrayList<>();
+
+        try {
+            st = cn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            rs = st.executeQuery("SELECT * FROM PRODUITS");
+
+            //On rajoute tous les produits
+            while(rs.next()){
+                Produit p = new Produit(rs.getString("nomProduit"),rs.getInt("prixProduit"),rs.getInt("qteProduit"));
+                arrayProduit.add(p);
+            }
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+
+        return arrayProduit;
+    }
+
+
+
+
 }
---
+++ src/uml/e05/monestier/dezette/metier/Catalogue.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uml.e05.monestier.dezette.metier;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author prax
+ */
+public class Catalogue implements I_Catalogue {
+
+    private List<I_Produit> Produits = new ArrayList<>();
+
+    @Override
+    public boolean addProduit(I_Produit produit) {
+      if(pdtOk(produit)){
+           Produits.add(produit);
+           return true;
+      }
+      return false;
+    }
+
+    @Override
+    public boolean addProduit(String nom, double prix, int qte) {
+        nom=nom.replaceAll("\t"," ");
+        Produit pdt = new Produit(nom.trim(),prix,qte);
+        if (pdtOk(pdt)) {
+            return Produits.add(pdt);
+        }
+        else{
+            
+        }
+        return false;
+    }
+
+    @Override
+    public int addProduits(List<I_Produit> l) {
+        int nbAjout = 0;
+        if (l == null) {
+            return 0;
+        } else {
+            for (I_Produit p : l) {
+                if(pdtOk(p)){
+                    
+                    Produits.add(p);
+                    nbAjout++;
+                }
+              
+                
+            }
+            return nbAjout;
+        }
+    }
+
+    @Override
+    public boolean removeProduit(String nom) {
+        boolean hasBeenRemoved = false;
+        int i = 0;
+        while (i < Produits.size() && hasBeenRemoved == false) {
+            if (Produits.get(i).getNom().equals(nom)) {
+                hasBeenRemoved = Produits.remove(Produits.get(i));
+            } else {
+                i++;
+            }
+
+        }
+
+        return hasBeenRemoved;
+    }
+
+    @Override
+    public boolean acheterStock(String nomProduit, int qteAchetee) {
+        if(produitExiste(nomProduit)){
+            I_Produit p = getProduit(nomProduit);
+            return p.ajouter(qteAchetee);
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public boolean vendreStock(String nomProduit, int qteVendue) {
+        I_Produit p = getProduit(nomProduit);
+        if(qteVendue>0 && qteVendue<p.getQuantite()){
+            return p.enlever(qteVendue);
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String[] getNomProduits() {
+
+        ArrayList<String> nomProduits = new ArrayList();
+
+        for (int i = 0; i < Produits.size(); i++) {
+            if (Produits.size() == 0) {
+                System.out.println("Vide!");
+            } else {
+                nomProduits.add(Produits.get(i).getNom()); 
+            }
+        }
+        Collections.sort(nomProduits);
+        String[] noms=new String[nomProduits.size()];
+        int i=0;
+        for(String s:nomProduits){
+            noms[i]=s;
+            noms[i]=noms[i].replaceAll("\t"," ");
+            noms[i]=noms[i].trim();
+            i++;
+        }
+        
+        return noms;
+    }
+
+    @Override
+    public double getMontantTotalTTC() {
+       if(Produits.size() == 0){
+            return 0.0;
+        }
+        java.text.DecimalFormat df = new java.text.DecimalFormat("0.00");
+        double prixTTC = 0;
+        for (I_Produit p : Produits ) {
+            System.out.println(""+p.getNom() + " qte: "+p.getQuantite() + "prixTTC" + p.getPrixUnitaireTTC());
+            prixTTC += p.getPrixUnitaireTTC() * p.getQuantite();        
+             
+        }
+ 
+        
+        return (double)Math.round(prixTTC*100)/100;
+    }
+
+    @Override
+    public void clear() {
+        Produits.clear();
+    }
+
+    @Override
+    public String toString() {
+        String sCatalogue = "";
+        for (I_Produit p:Produits) {
+            sCatalogue = sCatalogue + p.toString() + System.lineSeparator();
+        }
+        java.text.DecimalFormat df = new java.text.DecimalFormat("0.00");
+        sCatalogue = sCatalogue + System.lineSeparator() + "Montant total TTC du stock : " + df.format(this.getMontantTotalTTC()) + " €";
+        sCatalogue =  sCatalogue.replaceAll("\\.",",");
+        return sCatalogue;
+    }
+
+
+    public I_Produit getProduit(String nomPdt) {
+        int i = 0;
+        while (Produits.get(i).getNom().equals(nomPdt) == false && i < Produits.size() - 1) {
+            i++;
+        }
+        I_Produit p = Produits.get(i);
+        return p;
+    }
+
+    public boolean prixOk(double prixProduit) {
+
+        try {
+
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return (prixProduit > 0);
+    }
+
+    public boolean stockOk(int pStock) {
+        return pStock >= 0;
+    }
+
+    public boolean produitExiste(String nom) {
+        boolean res = false;
+        String[] nomPdt = getNomProduits();
+        int i = 0;
+        while (i < nomPdt.length && res == false) {
+            if (nomPdt[i].trim().equals(nom.trim())) {
+                res = true;
+            }
+            i++;
+        }
+        return res;
+    }
+
+    public boolean pdtOk(I_Produit pdt) {
+        boolean res = false;
+        if (pdt != null) {
+            System.out.println("Produit non nul");
+            if (produitExiste(pdt.getNom()) == false) {
+                 System.out.println("Produit similaire non existant");
+                if (prixOk(pdt.getPrixUnitaireHT())) {
+                     System.out.println("Prix positif");
+                    if (stockOk(pdt.getQuantite())) {
+                         System.out.println("Stock Positif");
+                        res = true; 
+                    }
+                    else{
+                        res = false;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}
